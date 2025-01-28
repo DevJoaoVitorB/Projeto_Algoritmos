d02e62e50d57e85342807ccbff313f312449b78e
João Vitor Bezerra <jv23072006@gmail.com>
2025-01-28 09:35:15 -0300

    Update CCode Bubble Sort

--- BubbleSort/CCode/bubblesort.c
+++ BubbleSort/CCode/bubblesort.c
@@ -5,13 +5,13 @@
 // Array que será ordenado pelo algoritmo Bubble Sort
 int *ordened_array;
 
-void bubble_sort(int qtd)
+void bubble_sort(int length_array)
 {
     int i, j, aux;
     
-    for(i = 0; i < qtd; i++)
+    for(i = 0; i < length_array; i++)
     {
-        for(j = 0; j < qtd-i-1; j++)
+        for(j = 0; j < length_array-i-1; j++)
         {      
             if(ordened_array[j] > ordened_array[j+1]) 
             {
@@ -25,38 +25,35 @@ void bubble_sort(int qtd)
 
 int main()
 {
-    int qtd_input = 0;
-    char name[50];
-    double init = 0, end = 0;
+    int length_array = 0;
+    char file_name[50];
+    double start = 0, end = 0;
 
     FILE* file;
 
     printf("Bubble Sort Algorithm \n");
     // Informa a quantidade de valores de entrada
     printf("Enter quantity of input values: ");
-    scanf("%d", &qtd_input);
-    ordened_array = (int *) malloc(qtd_input * sizeof(int));
+    scanf("%d", &length_array);
+    ordened_array = (int *) malloc(length_array * sizeof(int));
     // Informa o nome do arquivo
     printf("File Name: ");
-    scanf("%s", name);
+    scanf("%s", file_name);
 
     // Criar um arquivo com o nome informado
-    file = fopen(name, "w");
+    file = fopen(file_name, "w");
 
     // Gerar uma lista de X valores aleatorios(0 - 9,999)
-    for(int i = 0; i < qtd_input; i++)
+    for(int i = 0; i < length_array; i++)
     {
-        int value = rand() % 10000;
-        ordened_array[i] = value;
+        ordened_array[i] = rand() % 10000;
     }
 
     init = ((double) clock())/CLOCKS_PER_SEC; // Inicio do temporizador do algoritmo
-
     bubble_sort(qtd_input); // Chamada da função Bubble Sort
-
     end = (((double) clock())/CLOCKS_PER_SEC) - init; // Fim do temporizador do algoritmo
 
-    printf("%lf", end);
+    printf("%lf \n", end);
     
     // Escrever no arquivo o tempo de execução do algortimo 
     fprintf(file, "Runtime: %lfs", end);
